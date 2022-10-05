c4cbb19ee6d5fb43132ee34af06dc8373600d7a0
João P. Meneguesso <75391591+jpkm17@users.noreply.github.com>
2022-10-05 08:52:55 -0300

    Create biblioteca.sql

---
+++ biblioteca.sql
@@ -0,0 +1,18 @@
+create TABLE if not EXISTS tbl_livro( ID_Livro SMALLINT AUTO_INCREMENT PRIMARY KEY, 
+  Nome_Livro varchar (70) NOT null,
+  ISBN13 char(13), ISBN10 char(10), 
+  ID_Categoria SMALLINT, 
+  ID_Autor SMALLINT NOT null, 
+  Data_Pub date not null,
+  Preco_Livro decimal(6,2) NOT null 
+);
+
+CREATE table if not EXISTS tbl_autores(
+    ID_Autor SMALLINT PRIMARY KEY,
+    Nome_Autor varchar(50) not null,
+    Sobrenome_Autor varchar(60) not null
+);
+create table tbl_categoria(
+    ID_Categoria smallint(6) PRIMARY KEY AUTO_INCREMENT,
+    categoria varchar(30) not null
+);
